bf2e095a9c618277bb88f60ecacf4a274e902942
KocakAli <kocakali5834@gmail.com>
2023-01-11 00:18:24 +0300

    add show_personal_info method

--- classes/user.py
+++ classes/user.py
@@ -7,3 +7,11 @@ class User:
         self.name = name
         self.surname = surname
         self.age = age
+
+
+    def show_personal_info(self):
+        table = [
+            ["Name", "Surname", "Age", "Email"],
+            [self.name, self.surname, self.age, self.email]
+        ]
+        print(tabulate(table,headers="firstrow",tablefmt="fancy_grid"))
